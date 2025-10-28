8f8eddbf43368e1ec55149c9486600dc2dedbd3c
yezdom <developer@yezdom.com>
2025-10-28 23:50:13 +0000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { validateEnvironment } from './utils/validateEnv';
 
-validateEnvironment();
+// Temporarily disabled - causing app crash
+// import { validateEnvironment } from './utils/validateEnv';
+// validateEnvironment();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
